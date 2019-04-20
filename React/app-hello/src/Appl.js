0d79e4a31998ef7f7388e9905da17da9c918614b
Monicacholico <monicachol@hotmail.com>
2019-04-19 22:49:19 -0500

    working on chapter 22 of Moder React with Redux

---
+++ React/app-hello/src/Appl.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Appl = () => {
+    return <div>Hi There!</div>
+};
+
+reactDOM.render(
+    <App/>
+    document.querySelector("#root)
+);
