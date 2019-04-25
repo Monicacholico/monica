ae6b9b98a2f5abefe005803c539ddf3817899f3e
Monicacholico <monicachol@hotmail.com>
2019-04-25 17:27:54 -0500

    watching multiple props

--- React/components/src/CommentDetail.js
+++ React/components/src/CommentDetail.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 
 
-const CommentDetail =() =>{
+const CommentDetail =(props) =>{
     return (
         <div className="comment">
             <a href="/" className="avatar">
@@ -11,7 +11,7 @@ const CommentDetail =() =>{
             </a>
             <div className="content">
                 <a href="/" className="author">
-                    Sam
+                    {props.author}
                 </a>
                 <div className="metadata">
                     <span className="date"> Today at 6:00 pm</span>
