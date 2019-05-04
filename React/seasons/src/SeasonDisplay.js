5203802e13c4f42c54cc332e8151bacf082b8ffd
Monicacholico <monicachol@hotmail.com>
2019-05-03 21:01:26 -0500

    working on Section 6

--- React/seasons/src/SeasonDisplay.js
+++ React/seasons/src/SeasonDisplay.js
@@ -10,8 +10,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-    console.log(season);
-    return <div>Season Display</div>;
+    const text = season === "Winter" ? "Burr, it is chilly" : "Let's hit the beach";
+    const icon = season === "winter" ? "snowflake" : "sun";
+    return (
+    <div>
+        <i className={`${icon} icon`} />
+        <h1>{text}</h1>
+        <i className={`${icon} icon`} />
+    </div>
+    );
 };
 
 export default SeasonDisplay;
