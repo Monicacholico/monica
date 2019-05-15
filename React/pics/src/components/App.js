a03e55a7fa5312efdfb9052ce937d3fb2a97544d
Monicacholico <monicachol@hotmail.com>
2019-05-14 22:34:20 -0500

    working on section 7 Modern React with Redux

--- React/pics/src/components/App.js
+++ React/pics/src/components/App.js
@@ -3,9 +3,11 @@ import SearchBar from './SearchBar';
 
 
 const App = () => {
-    return <div>
-        <SearchBar/>
-        </div>;
+    return(
+        <div className="ui container" style={{marginTop: '10px'}}>
+            <SearchBar/>
+        </div>
+        )
 };
 
 export default App;
--- React/pics/src/components/SearchBar.js
+++ React/pics/src/components/SearchBar.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 
+
 class SearchBar extends Component {
+    state = {term: ''};
+    // onInputChange(event){
+    //    console.log(event.target.value)
+    // }
+    onFormSubmit(e) {
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <div>
-                <form>
-                    <input type="text"/>
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label htmlFor="">
+                            Image Search
+                        </label>
+                        <input type="text"
+                               value={this.state.term}
+                               onChange={e => this.setState({term: e.target.value})}
+                        />
+                    </div>
                 </form>
             </div>
+
         );
     }
 }
