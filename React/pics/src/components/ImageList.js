47a6d0ca64404177c8307f77f36094d367ef7892
Monicacholico <monicachol@hotmail.com>
2019-05-21 23:36:08 -0500

    working on section 10 MOdern React with Redux

--- React/pics/src/components/ImageList.js
+++ React/pics/src/components/ImageList.js
@@ -5,8 +5,7 @@ import ImageCard from './ImageCard';
 const ImageList = (props) => {
    const images = props.images.map((image) => {
        return <ImageCard key={image.id}
-                         image={image}
-       />
+                         image={image}/>
    });
 
     return <div className="image-list">{images}</div>;
