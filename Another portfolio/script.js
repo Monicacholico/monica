a7e69ecdb382f197b8e8c5a71b12ac33629861b6
Monicacholico <monicachol@hotmail.com>
2019-08-01 23:55:57 -0500

    added javascript file

---
+++ Another portfolio/script.js
@@ -0,0 +1,15 @@
+function onTabClick(e){
+    let activeTabs = document.querySelectorAll('.active');
+
+    activeTabs.forEach(function(tab){
+        tab.className = tab.className.replace('active', '');
+    })
+
+    e.target.parentElement.className += ' active';
+    document.getElementById(event.target.href.split("#")[1]).className += 'active';
+}
+
+const element = document.getElementById('nav-tab');
+
+element.addEventListener('click', onTabClick, false);
+
