34fb918a57f5f7cb540ed10ab142440ed91b6b96
Monicacholico <monicachol@hotmail.com>
2019-08-02 22:01:24 -0500

    added javascript to drag

--- Another portfolio/script.js
+++ Another portfolio/script.js
@@ -3,13 +3,58 @@ function onTabClick(e){
 
     activeTabs.forEach(function(tab){
         tab.className = tab.className.replace('active', '');
-    })
+    });
 
     e.target.parentElement.className += ' active';
-    document.getElementById(event.target.href.split("#")[1]).className += 'active';
+    document.getElementById(event.target.href.split("#")[1]).className += ' active';
 }
 
 const element = document.getElementById('nav-tab');
 
 element.addEventListener('click', onTabClick, false);
 
+// For Drag and Drop //
+
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+
+
+for(const empty of empties){
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+
+function dragStart(){
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd(){
+    this.className = 'fill';
+}
+
+function dragOver(e){
+    e.preventDefault();
+}
+
+function dragEnter(e){
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave(){
+    this.className = 'empty';
+}
+
+function dragDrop(){
+    this.className = 'empty';
+    this.append(fill);
+
+}
