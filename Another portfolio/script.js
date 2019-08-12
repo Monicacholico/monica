a581c101fccceb307b081ded19c7bdf55a59f269
Monicacholico <monicachol@hotmail.com>
2019-08-11 22:53:49 -0500

    another to do list

--- Another portfolio/script.js
+++ Another portfolio/script.js
@@ -89,6 +89,17 @@ function myToDoList() {
 }
 
 
+//////////        Large To Do List ////////////
+
+var myNodelist = document.getElementsByTagName('li');
+var i;
+for(i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement('span');
+    var txt = document.createTextNode('\u00D7');
+    span.className = 'close';
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
 
 
 
