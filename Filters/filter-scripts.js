b41abbfc095d676aef3a992b6cc335a1cafe4ac6
Monicacholico <monicachol@hotmail.com>
2019-10-07 22:57:15 -0500

    first prototype for filter

---
+++ Filters/filter-scripts.js
@@ -0,0 +1,69 @@
+
+pointeShoes = [
+    {
+        name: 'Alpha',
+        brand: 'Bloch',
+        characteristics: [
+            {
+                level: 'Advanced'
+            },
+            {   toeLengths: 'Long'
+
+            },
+            {
+                strength: 'Medium'
+            },
+            {
+                profile : 'High'
+            },
+            {
+                width: 'Narrow'
+            },
+            {
+                footType: 'Egyptian'
+            }
+
+        ]
+    },
+    {
+        name: 'Elite',
+        brand: 'Grishko',
+        characteristics: [
+            {
+                level: 'Medium'
+            },
+            {   toeLengths: 'Short'
+
+            },
+            {
+                strength: 'Medium'
+            },
+            {
+                profile : 'Low'
+            },
+            {
+                width: 'Wide'
+            },
+            {
+                footType: 'Giselle'
+            }
+
+        ]
+    }
+];
+
+
+let filterPointeShoes = pointeShoes.filter(pointeShoe => pointeShoe.characteristics[0].level.includes('Med'));
+
+console.log(filterPointeShoes);
+
+let pointe = document.querySelector('.result');
+console.log(pointe);
+
+let pointeString = JSON.stringify(filterPointeShoes[0].name).replace(/\"/g, "" );
+
+// pointe.innerHTML = pointeShoes.brand + ' ' + pointeShoes.name + " " + pointeShoes.characteristics;
+
+pointe.innerHTML = pointeString;
+
+
