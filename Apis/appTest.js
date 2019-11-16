f43119fcfd019c16b348715c5557877459533a1b
Monicacholico <monicachol@hotmail.com>
2019-11-16 12:54:32 -0600

    api pracice

---
+++ Apis/appTest.js
@@ -0,0 +1,21 @@
+
+
+https://api.instagram.com/oauth/authorize
+?app_id=505690273354751
+&redirect_uri=https%3A%2F%2Fmonicacholico.github.io%2F
+&scope=user_profile,user_media
+&response_type=code
+
+
+https://api.instagram.com/oauth/authorize
+        ?app_id=725433097973313
+    &redirect_uri=https%3A%2F%2Fmonicacholico.github.io%2F
+    &scope=user_profile,user_media
+&response_type=code
+
+
+    ?code=AQAFFz2mZA3LYCsVmNkM-05W4Q1nGc1vHPSKaCYDlsjHsyQEVFQwoXuLN96jgBIV6Lq23saCnYsFFZiBWp2CT7WenuxwBR7RwaKXhT9VnGsSTzv1c0fQIO7233u7BnRP7kC2x2qjMqW0uBcXaM0f0GOSKT3dkcU1VYhmtCtol_Dr_h5v5ByxhWsSSPP-nJMDUx2aqQYuCCdQ731KyCCvi869YXK1kLQZGUW3pG_HaLBjMA#_
+
+
+
+https://api.instagram.com/oauth/authorize\?app_id=725433097973313\redirect_uri=https://monicacholico.github.io/\scope=user_profile,user_media\response_type=AQAFFz2mZA3LYCsVmNkM-05W4Q1nGc1vHPSKaCYDlsjHsyQEVFQwoXuLN96jgBIV6Lq23saCnYsFFZiBWp2CT7WenuxwBR7RwaKXhT9VnGsSTzv1c0fQIO7233u7BnRP7kC2x2qjMqW0uBcXaM0f0GOSKT3dkcU1VYhmtCtol_Dr_h5v5ByxhWsSSPP-nJMDUx2aqQYuCCdQ731KyCCvi869YXK1kLQZGUW3pG_HaLBjMA#_
