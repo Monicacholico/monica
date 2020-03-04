3dc6356cd071a5878ca397e3adfcc8b5ccee714d
Monicacholico <monicachol@hotmail.com>
2020-03-04 16:15:53 -0600

    added object banner

--- CSS animations/Intro/banner.js
+++ CSS animations/Intro/banner.js
@@ -1,21 +1,46 @@
-// function greeting(){
-//     alert('Helloooooo piiii');
-// }
-
-// greeting();
 
 class Banner{
     constructor(balletBanner){
         this.title = balletBanner.title;
         this.imageUrl = balletBanner.image;
+        this.background = balletBanner.background;
+        this.ballerina = balletBanner.ballerina;
         this.description = balletBanner.description;
+        this.setBackground();
+        this.buildBanner();
     }
+    
+    setBackground() {
+        const backgroundHeader = document.getElementById('app');
+        console.log(backgroundHeader);
+        backgroundHeader.style.backgroundImage = `url(${this.background})`
+    }
+
+
+    buildBanner(){
+        this.setBackground();
+        const html = `<header class="header">
+        <h2 class="title">${this.title}</h2>
+        <img src="${this.ballerina}" alt="" id="main-image">
+        <img src="${this.imageUrl}" alt="" id="trees">
+        <p class="featured-desc">${this.description}</p>
+        </header>`;
+        document.getElementById('app').innerHTML += html;
+    }
+    
+
 }
 
-const bannerList = {
-    banners: [
-        new Banner (
-            
-        )
-    ]
-}
+new Banner({
+    title: 'Maria Kochetkova',
+    ballerina: 'bird.png',
+    background: 'background1.jpg',
+    description: 'Famous Russian Ballerina'
+})
+// const bannerList = {
+//     banners: [
+//         new Banner (
+
+//         )
+//     ]
+// }
