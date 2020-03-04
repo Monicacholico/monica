60d848fb55fd2a1416f9c68ad1e221226acff87b
Monicacholico <monicachol@hotmail.com>
2020-03-03 21:42:16 -0600

    working on objects

---
+++ CSS animations/Intro/banner.js
@@ -0,0 +1,21 @@
+// function greeting(){
+//     alert('Helloooooo piiii');
+// }
+
+// greeting();
+
+class Banner{
+    constructor(balletBanner){
+        this.title = balletBanner.title;
+        this.imageUrl = balletBanner.image;
+        this.description = balletBanner.description;
+    }
+}
+
+const bannerList = {
+    banners: [
+        new Banner (
+            
+        )
+    ]
+}
