a87355ca91dd7a01d1287e99cc42b84ea5237a1e
Monicacholico <monicachol@hotmail.com>
2020-03-07 20:17:35 -0600

    weird javascript

---
+++ Weird Javascript/app.js
@@ -0,0 +1,115 @@
+
+
+
+function Person(name, lastName) {
+    this.name = name;
+    this.lastName = lastName;
+}
+
+Person.prototype.getFullName = function(){
+    return this.name + ' ' + this.lastName;
+}
+
+
+let monica = new Person('Monica','Lopez')
+console.log(monica);
+console.log(monica.getFullName());
+
+
+
+function AnotherPerson ( personObject){
+    this.name = personObject.name;
+    this.lastName = personObject.lastName;
+}
+
+const josh = new AnotherPerson({
+    name: 'Josh',
+    lastName: 'Maher'
+})
+
+// console.log(josh);
+
+
+class NewPerson{
+    constructor(personObject){
+        this.name = personObject.name;
+        this.lastName = personObject.lastName;
+    }
+}
+
+const newOtherPerson = new NewPerson({
+    name: 'Julian',
+    lastName: 'Salas'
+})
+
+// console.log(newOtherPerson);
+
+
+
+
+
+const objectMaker = document.getElementById('theButton');
+objectMaker.addEventListener('click', function(){
+    let name = document.getElementById('my-name').value;
+    let lastName = document.getElementById('my-lastname').value;
+    const newOtherPerson = new NewPerson({
+        name: name,
+        lastName: lastName
+    })
+    console.log( newOtherPerson);
+    
+    
+
+
+})
+
+
+
+String.prototype.isLengthGreaterThan = function(limit) {
+    return this.length > limit;
+}
+
+console.log('John'.isLengthGreaterThan(3));
+
+
+
+// Arrays prototypes with for in
+
+// Array.prototype.customFunction = 
+
+var arr = ['John', 'Jane', 'Jim'];
+
+
+for(var prop in arr) {
+    console.log( prop + ': ' + arr[prop]);
+}
+
+console.log(arr);
+
+
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    greet: function(){
+        return 'Hi ' + this.firstname;
+    }
+}
+
+var john = Object.create(person);
+john.firstname = 'John';
+john.lastname = 'Doe';
+console.log(john);
+
+
+// POLYFILL
+
+if(!Object.create) {
+    Object.create = function(o) {
+        if(arguments.lenght > 1 ){
+            throw new Error('Object.create impleentation' + ' only accepts the sifrst parameter.');
+        }
+        function F(){}
+            F.prototype = o;
+            return new F();
+        };
+}
