4c0b5b4b86613b6197862247cd49368048a5de78
Monicacholico <monicachol@hotmail.com>
2020-06-23 00:20:35 -0500

    practicing oop with tabs

--- Tabs/verticalTabs/index.js
+++ Tabs/verticalTabs/index.js
@@ -2,7 +2,6 @@
 function Tabs(element) {
     this.tabsElement = element;
     this.tabListElem = element.querySelector('.vertical-tabs-list-wrap');
-    
     this.tabContentWrap = element.querySelector('.vertical-tabs-content-wrap');
     this.tabGroupName = element.dataset.tabName || 'tabGroup';
     this.allTabs = element.querySelectorAll('.vertical-tab');
@@ -30,9 +29,62 @@ Tabs.prototype.handleKeyboardEnter = function(e) {
 Tabs.prototype.setActiveTab = function(tabElem) {
     if(!tabElem) return;
     if(this.tabListElem.querySelector('.vertical-tab-activ .vertical-tab-link') === tabElem) return;
-
+    //remove active
     this.tabListElem.querySelector('.vertical-tab-active .vertical-tab-link').setAttribute('aria-selected', false);
     this.tabListElem.querySelector('.vertical-tab-content.active').classList.remove('active');
-    var allTabs = [].slice.call(this.allTabs)
-    console.log(allTabs);
+    // set active
+    tabElem.setAttribute('aria-selected', true);
+    tabElem.classList.add('vertical-tab-active');
+    this.tabContentWrap.querySelector('#' + tabElem.dataset.tabId).classList.add('active');
+    var allTabs = [].slice.call(this.allTabs);
+    this.activeTabIndex = this.findIndex(allTabs, function(tab){
+        return tab.classList.contains('vertical-tab-active');
+    });
 }
+
+Tabs.prototype.handleKeyBoardNav = function(e) {
+    var KEYCODE = {
+        UP_ARROW: 38,
+        DOWN_ARROW: 40,
+        HOME_KEY: 36,
+        END_KEY: 35
+    };
+
+    var allTabs = [].slice.call(this.allTabs);
+    var currentTabIndex = this.findIndex(allTabs, function(tab) {
+        return tab.classList.contains('vertical-tab-active');
+    });
+    var currentTab = allTabs[currentTabIndex];
+    var firstTab = allTabs[0] || currentTab;
+    var lastTab = allTabs[this.allTabs.length - 1] || currentTab;
+    var nextTab = allTabs[currentTabIndex + 1] || firstTab;
+    var prevTab = allTabs[currentTabIndex - 1] || lastTab;
+
+    switch (e.keyCode) {
+        case KEYCODE.UP_ARROW:
+            e.preventDefault();
+            this.setActiveTab(prevTab.querySelector('.vertical-tab-link'));
+            return;
+        case KEYCODE.DOWN_ARROW:
+            e.preventDefault();
+            this.setActiveTab(nextTab.querySelector('.vertical-tab-link'));
+            return;
+        case KEYCODE.HOME_KEY:
+            e.preventDefault();
+            this.setActiveTab(firstTab.querySelector('.vertical-tab-link'));
+            return;
+        case KEYCODE.END_KEY:
+            e.preventDefault();
+            this.setActiveTab(lastTab.querySelector('.vertical-tab-link'));
+            return;
+        default:
+            return;
+    }
+};
+
+(function() {
+    var tabsElemets = document.querySelectorAll('.cdo .vertical-tabs');
+    Array.prototype.slice.call(tabsElemets).forEach(function (elem) {
+        new Tabs(elem);
+    });
+})();
