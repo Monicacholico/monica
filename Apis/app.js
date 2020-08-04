c06fb27b7f8f71e9b2c1a4a84b4382fe76a76c8f
Monicacholico <monicachol@hotmail.com>
2020-08-03 23:53:24 -0500

    parsed json from api pointeshoes

--- Apis/app.js
+++ Apis/app.js
@@ -10,9 +10,24 @@
 //     });
 
 async function fetchingPointes(){
+    const listPointeShoes = document.querySelector('.pointeShoes');
+    const pointeShoe = document.getElementById('post-shoe');
+    const wrapper = document.getElementById('postWrapper');
+    console.log(wrapper);
     const response = await fetch("data.json");
     const data = await response.json();
-    console.log(data);
-    document.getElementById('featuredProfile').innerText = JSON.stringify(data);
+    console.log(data.pointeShoes);
+    // document.getElementById('featuredProfile').innerText = JSON.stringify(data);
+    const allPointeShoes = data.pointeShoes;
+    console.log(allPointeShoes)
+    for (const pointeShoe of allPointeShoes){
+        const pointeEl = document.importNode(wrapper.content, true);
+        pointeEl.querySelector('h2').textContent = pointeShoe.name.toUpperCase();
+        console.log(pointeEl.querySelector('p'));
+        pointeEl.querySelector('p').textContent = pointeShoe.level;
+        listPointeShoes.append(pointeEl);
+    }
 }
 fetchingPointes();
+
+
