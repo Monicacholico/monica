4803378299ac1d623c1a768451b1496b04016fc3
Monicacholico <monicachol@hotmail>
2020-08-05 20:55:05 -0500

    saving

--- Apis/app.js
+++ Apis/app.js
@@ -6,13 +6,13 @@
 //     console.log(data.pointeShoes);
 // const div = document.getElementById('featuredProfile');
 // console.log(div);
-// div.innerText = data.pointeShoes;
+// div.innerText = JSON.stringify(data.pointeShoes);
 //     });
 
 async function fetchingPointes(){
-    const response = await fetch("data.json");
+    const response = await fetch("./data.json");
     const data = await response.json();
     console.log(data);
-    document.getElementById('featuredProfile').innerText = JSON.stringify(data);
+    document.getElementById('featuredProfile').innerText = data.pointeShoes[1].name;
 }
 fetchingPointes();
