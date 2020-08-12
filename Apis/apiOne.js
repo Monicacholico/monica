bbe9bacdba6308944d67d3559fe4a1e99ab9c600
Monicacholico <monicachol@hotmail>
2020-08-11 23:24:06 -0500

    adding movie reviews to api data

--- Apis/apiOne.js
+++ Apis/apiOne.js
@@ -4,7 +4,7 @@ const reviewTemplate = document.getElementById('review-item');
 const xhr = new XMLHttpRequest();
 
 const API_N = 'UbsMbKtd9JKyBhGjMGGnrcMHjJFctMKw';
-xhr.open('GET', `https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=${API_N}`);
+xhr.open('GET', `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=${API_N}`);
 
 xhr.responseType = 'json';
 
@@ -13,8 +13,9 @@ xhr.onload = function() {
   console.log(listOfReviews);
   for (const review of listOfReviews) {
     const reviewEl = document.importNode(reviewTemplate.content, true);
-    reviewEl.querySelector('h2').textContent = review.display_name;
-    reviewEl.querySelector('p').textContent = review.updated;
+    reviewEl.querySelector('h2').textContent = review.title;
+    reviewEl.querySelector('p').textContent = review.description;
+    reviewEl.querySelector('small').textContent = review.author;
     reviewElement.append(reviewEl);
   }
 }
@@ -27,33 +28,33 @@ xhr.send();
 
   /////////////// WITH FETCH //////////////////
     
-  //   function execute() {
-  //   const url = 'https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=UbsMbKtd9JKyBhGjMGGnrcMHjJFctMKw';
-  //   const options = {
-  //     method: "GET",
-  //     headers: {
-  //       "Accept": "application/json"
-  //     },
-  //   };
-  //   fetch(url, options).then(
-  //     response => {
-  //       if (response.ok) {
-  //         return response.text();
-  //       }
-  //       return response.text().then(err => {
-  //         return Promise.reject({
-  //           status: response.status,
-  //           statusText: response.statusText,
-  //           errorMessage: err,
-  //         });
-  //       });
-  //     })
-  //     .then(data => {
-  //       console.log(data);
-  //     })
-  //     .catch(err => {
-  //       console.error(err);
-  //     });
-  // }
+    function execute() {
+    const url = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=UbsMbKtd9JKyBhGjMGGnrcMHjJFctMKw';
+    const options = {
+      method: "GET",
+      headers: {
+        "Accept": "application/json"
+      },
+    };
+    fetch(url, options).then(
+      response => {
+        if (response.ok) {
+          return response.text();
+        }
+        return response.text().then(err => {
+          return Promise.reject({
+            status: response.status,
+            statusText: response.statusText,
+            errorMessage: err,
+          });
+        });
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
   
-  // execute()
+  execute()
