42f34f8a244330d8163f084b16dfb64248b63628
Monicacholico <monicachol@hotmail>
2020-11-11 17:54:27 -0600

    refactor carousel script

--- carousel/carousel-refactor.js
+++ carousel/carousel-refactor.js
@@ -1,38 +1,77 @@
 const slideContainer = document.querySelector('.slideshow-container');
 
-console.log(slideContainer);
-
-
-
 const slides = document.querySelectorAll('.mySlides');
 const allSlides = Array.from(slides);
-console.log(allSlides);
-
-
-function setActiveSlide () {
-    let firstSlide = document.getElementById('slide-one');
-    let current = document.querySelector('.mySlides.active');
-    allSlides.forEach( slide => {
-        if(slide === current) {
-            slide.classList.remove('active')
-        }
-   let nextCurrent = current.nextElementSibling.classList.add('active');
-        if(nextCurrent) {
-            nextCurrent = current;
-        } 
-    });
-}
-
-
-
-
-
 
 const prevBtn = document.querySelector('.prev');
-console.log(prevBtn);
 
 const nextBtn = document.querySelector('.next');
-nextBtn.addEventListener('click', setActiveSlide);
+
+let current = 0;
+
+// clear all images
+function reset() {
+    allSlides.forEach( slide => {
+        slide.classList.remove('active');
+    })
+}
+
+// Init slider
+function startSlide() {
+    reset();
+    allSlides[0].classList.add('active');
+}
+
+// Show prev
+function slideLeft() {
+    reset();
+    allSlides[current - 1].classList.add('active');
+    current--;
+}
+
+prevBtn.addEventListener('click', function() {
+    if(current === 0) {
+        current = allSlides.length;
+    }
+    slideLeft();
+} );
+
+
+// Show next
+function slideRight() {
+    reset();
+    allSlides[current + 1].classList.add('active');
+    current++;
+}
+
+nextBtn.addEventListener('click', function() {
+    if(current === allSlides.length -1) {
+        current = -1;
+    }
+    slideRight();
+} );
+
+
+
+
+startSlide();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
