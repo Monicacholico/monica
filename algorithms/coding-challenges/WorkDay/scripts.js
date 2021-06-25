dc2476a217c257101e53d6531b2ceb58cc76cc6e
Monicacholico <monicachol@hotmail.com>
2021-06-24 23:08:55 -0700

    bug

--- algorithms/coding-challenges/WorkDay/scripts.js
+++ algorithms/coding-challenges/WorkDay/scripts.js
@@ -3,19 +3,17 @@ const allSlides = [...slides];
 
 let i = 0;
 const slideShow = () => {
-    // allSlides[i].style.display = 'block';
     allSlides[i].style.display = 'none';
     if(i < allSlides.length - 1) {
+        console.log(i);
         console.log(allSlides[i]);
-        // allSlides[i].style.display = 'block';
-        // console.log(allSlides[i])
         i++;
         allSlides[i].style.display = 'block';
     } else {
         i = 0;
     }
 }
-setInterval(slideShow, 2000);
+setInterval(slideShow, 3000);
 
 
 
