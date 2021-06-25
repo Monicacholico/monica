2672e6a1397a713705afe5a5fdaa8aadb9bf9997
Monicacholico <monicachol@hotmail>
2021-06-24 19:34:32 -0500

    added exercise

---
+++ algorithms/coding-challenges/WorkDay/scripts.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.Slides__block');
+const allSlides = [...slides];
+
+let i = 0;
+const slideShow = () => {
+    allSlides[i].style.display = 'none';
+    if(i < allSlides.length - 1) {
+        // console.log(allSlides[i])
+        i++;
+        allSlides[i].style.display = 'block';
+    } else {
+        i = 0;
+    }
+}
+setInterval(slideShow, 2000);
+
+
+
+
