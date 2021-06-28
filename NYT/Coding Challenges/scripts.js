40f12e3b76a37ebc8c7f8567fcb7d31e1360316e
Monicacholico <monicachol@hotmail>
2021-06-27 22:57:54 -0500

    added coding challenge

--- NYT/Coding Challenges/scripts.js
+++ NYT/Coding Challenges/scripts.js
@@ -4,9 +4,9 @@ function isBalanced(string) {
         let char = stack[stack.length - 1];
         if(string[i] === '(' || string[i] === '[' || string[i] === '{'){
             stack.push(string[i]);
-        } else if ((char === '(' && string[i] === ')') || 
+        } else if ((char === '(' && string[i] === ')') ||
         (char === '[' && string[i] === ']') ||
-        (char === '{' && string[i]=== '}') 
+        (char === '{' && string[i]=== '}')
         ){
             stack.pop();
         } else {
@@ -17,3 +17,53 @@ function isBalanced(string) {
 }
 
 console.log(isBalanced('({[]})'))
+
+function removeDuplicates(array) {
+
+    const noDup = [...new Set(array)];
+    return noDup;
+
+}
+
+function removeDuplicates2(array) {
+    const object = {};
+    for(let element of array) {
+        object[element] = object[element] + 1 || 1;
+
+    }
+
+    for(let element of array) {
+        if(object[array[element]] > 1) {
+            object[array[element]] = 1;
+        }
+    }
+    const newArray = Object.keys(object).map( element => Number(element));
+
+    console.log(object);
+    return newArray;
+}
+
+console.log(removeDuplicates([1,2,3,4,3,3,4,5,5,6,7,8]))
+console.log(removeDuplicates2([1,2,3,4,3,3,4,5,5,6,7,8]));
+
+
+function parenthesisQ(string) {
+    let stack = [];
+    for(let i = 0; i < string.length; i++) {
+        let char = stack[stack.length - 1];
+        if(string[i] === '(' || string[i] === '[' || string[i] === '{'){
+            stack.push(string[i]);
+        } else if((char === '(' && string[i] === ')') ||
+            (char === '[' && string[i] === ']') ||
+            (char === '{' && string[i] === '}')
+        ) {
+            stack.pop();
+        } else {
+            return false;
+        }
+        console.log(stack);
+    }
+    return stack.length ? false : true;
+}
+
+console.log(parenthesisQ('([])'))
